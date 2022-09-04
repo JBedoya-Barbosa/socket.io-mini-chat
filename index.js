15845bf6ed61962745e0bde72f7043ce435fb7cc
JBedoyaBarbosa <juanbedoya.barbosa@gmail.com>
2022-09-03 20:36:28 -0500

    commit

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.static(path.resolve(__dirname + '/public')))
 
 server.listen(PORT, () => console.log(`[+] Server running on port ${PORT}`))
